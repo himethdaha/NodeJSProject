87ceb25e68ae086502b3a72101882c0bbf49e2c3
Himeth Dahanayake <himethdaha@gmail.com>
2022-02-07 14:51:10 -0500

    Seperating the callbacks from the routes

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 //Importing the Express module
-const e = require('express');
 const express = require('express');
 //Importing the FS module
 const fs = require('fs');
-
+const tourController = require('./controllers/tourController');
 //Calling the exported express function
 const app = express();
 //Middleware
@@ -16,128 +15,19 @@ const tours = JSON.parse(
 
 //***ROUTES***//
 //Get all tours
-app.get('/api/v1/tours', (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    results: tours.length,
-    data: {
-      tours: tours,
-    },
-  });
-});
+app.get('/api/v1/tours', tourController.getTours);
 
 //Get a tour
-app.get('/api/v1/tours/:id', (req, res) => {
-  //Get the id
-  const id = Number(req.params.id);
-  //Find the element
-  const element = tours.find((el) => el.id === id);
-
-  //Return 404 if invalid id
-  if (!element) {
-    return res.status(404).json({
-      status: 'Fail',
-      message: 'Invalid Id',
-    });
-  } else {
-    return res.status(200).json({
-      status: 'Success',
-      tour: element,
-    });
-  }
-});
+app.get('/api/v1/tours/:id', tourController.getTour);
 
 //Post a tour
-app.post('/api/v1/tours', (req, res) => {
-  //Get the request body
-  const reqBody = req.body;
-  //Assign the id for the new post
-  const id = tours.length - 1 + 1;
-  //Create the object
-  const newObj = Object.assign({ id }, { ...reqBody });
-  //Push the new Object to the tours array
-  tours.push(newObj);
-  //Re-write the tours array to the JSON file
-  fs.writeFile(
-    './dev-data/data/test-data.json',
-    JSON.stringify(tours),
-    (err) => {
-      console.log('writing');
-    }
-  );
-  res.status(201).json({
-    status: 'success',
-    data: {
-      tour: newObj,
-    },
-  });
-});
+app.post('/api/v1/tours', tourController.postTour);
 
 //Patch a tour
-app.patch('/api/v1/tours/:id', (req, res) => {
-  //Get the id
-  const id = Number(req.params.id);
-  //Find the element
-  const element = tours.find((el) => el.id === id);
-  let returnedObj;
-  //Return 404 if invalid id
-  if (!element) {
-    return res.status(404).json({
-      status: 'Fail',
-      message: 'Invalid Id',
-    });
-  } else {
-    //Create new Object with the id and request body
-    const patchTour = Object.assign({ id: id }, req.body);
-
-    //Change the element object
-    Object.keys(patchTour).map((key) => {
-      //Check for the keys of the element object that matches with the patch object
-      if (element.hasOwnProperty(key)) {
-        returnedObj = Object.assign(element, patchTour);
-
-        //Remove the element and add the returnedObj to the array
-        tours.splice(element.id, 1, returnedObj);
-      }
-    });
-  }
-
-  //Persist the returnedObj
-  fs.writeFile(
-    './dev-data/data/test-data.json',
-    JSON.stringify(tours),
-    (err) => {
-      res.status(200).json({
-        status: 'Success',
-        data: {
-          tours: returnedObj,
-        },
-      });
-    }
-  );
-});
+app.patch('/api/v1/tours/:id', tourController.patchTour);
 
 //Delete a Tour
-app.delete('/api/v1/tours/:id', (req, res) => {
-  //Get the id
-  const id = Number(req.params.id);
-  //Find the element
-  const element = tours.find((el) => el.id === id);
-  //Validate the id
-  if (!element) {
-    return res.status(404).json({
-      status: 'Fail',
-      message: 'Invalid Id',
-    });
-  } else {
-    //Remove the element
-    tours.splice(element.id, 1);
-    res.status(204).json({
-      status: 'success',
-      data: null,
-    });
-  }
-});
+app.delete('/api/v1/tours/:id', tourController.deleteTour);
 const port = 3000;
 //Create the server
 app.listen(port, () => {
---
+++ controllers/tourController.js
@@ -0,0 +1,122 @@
+//Importing the FS module
+const fs = require('fs');
+//Read the test data
+const tours = JSON.parse(
+  fs.readFileSync('./dev-data/data/test-data.json', 'utf-8')
+);
+
+//Callback functions for the Routes
+exports.getTours = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours: tours,
+    },
+  });
+};
+exports.getTour = (req, res) => {
+  //Get the id
+  const id = Number(req.params.id);
+  //Find the element
+  const element = tours.find((el) => el.id === id);
+
+  //Return 404 if invalid id
+  if (!element) {
+    return res.status(404).json({
+      status: 'Fail',
+      message: 'Invalid Id',
+    });
+  } else {
+    return res.status(200).json({
+      status: 'Success',
+      tour: element,
+    });
+  }
+};
+exports.postTour = (req, res) => {
+  //Get the request body
+  const reqBody = req.body;
+  //Assign the id for the new post
+  const id = tours.length - 1 + 1;
+  //Create the object
+  const newObj = Object.assign({ id }, { ...reqBody });
+  //Push the new Object to the tours array
+  tours.push(newObj);
+  //Re-write the tours array to the JSON file
+  fs.writeFile(
+    './dev-data/data/test-data.json',
+    JSON.stringify(tours),
+    (err) => {
+      console.log('writing');
+    }
+  );
+  res.status(201).json({
+    status: 'success',
+    data: {
+      tour: newObj,
+    },
+  });
+};
+exports.patchTour = (req, res) => {
+  //Get the id
+  const id = Number(req.params.id);
+  //Find the element
+  const element = tours.find((el) => el.id === id);
+  let returnedObj;
+  //Return 404 if invalid id
+  if (!element) {
+    return res.status(404).json({
+      status: 'Fail',
+      message: 'Invalid Id',
+    });
+  } else {
+    //Create new Object with the id and request body
+    const patchTour = Object.assign({ id: id }, req.body);
+
+    //Change the element object
+    Object.keys(patchTour).map((key) => {
+      //Check for the keys of the element object that matches with the patch object
+      if (element.hasOwnProperty(key)) {
+        returnedObj = Object.assign(element, patchTour);
+
+        //Remove the element and add the returnedObj to the array
+        tours.splice(element.id, 1, returnedObj);
+      }
+    });
+  }
+
+  //Persist the returnedObj
+  fs.writeFile(
+    './dev-data/data/test-data.json',
+    JSON.stringify(tours),
+    (err) => {
+      res.status(200).json({
+        status: 'Success',
+        data: {
+          tours: returnedObj,
+        },
+      });
+    }
+  );
+};
+exports.deleteTour = (req, res) => {
+  //Get the id
+  const id = Number(req.params.id);
+  //Find the element
+  const element = tours.find((el) => el.id === id);
+  //Validate the id
+  if (!element) {
+    return res.status(404).json({
+      status: 'Fail',
+      message: 'Invalid Id',
+    });
+  } else {
+    //Remove the element
+    tours.splice(element.id, 1);
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  }
+};
