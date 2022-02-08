a9126a2144e8eb5f721c3bbe9a701563f64b38bb
Himeth Dahanayake <himethdaha@gmail.com>
2022-02-08 18:07:53 -0500

    Creating a test Schema and a Model

--- server.js
+++ server.js
@@ -23,6 +23,25 @@ mongoose
   .then((conn) => {
     console.log('Connected to the db');
   });
+//Creating a schema
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A Name is required'],
+    unique: true,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A Price is required'],
+  },
+  rating: {
+    type: Number,
+    default: 4.0,
+  },
+});
+
+//Creating a model
+const Tour = mongoose.model('Tour', tourSchema);
 
 const port = process.env.PORT;
 //Create the server
