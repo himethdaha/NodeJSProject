bf09d86e32a883b0ec7d785ea0f16a58a545e25e
Himeth Dahanayake <himethdaha@gmail.com>
2022-02-08 17:34:19 -0500

    Adding connections to mongoose atlas via my app and mongosh

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
+//THIS IS WHERE THE SETUP OF MY APPLICATION HAPPENS
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
-
 const app = require('./app');
 
-console.log(process.env);
+//Get the database string to connect the app with atlas
+const DbString = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+//Conenct to the database using mongoose
+//connect returns a promise which is a connection object
+mongoose
+  .connect(DbString, {
+    //To deal with deprecation warnings
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then((conn) => {
+    console.log('Connected to the db');
+  });
+
 const port = process.env.PORT;
 //Create the server
 app.listen(port, () => {
