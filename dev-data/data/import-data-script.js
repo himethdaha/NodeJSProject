c079e6e9c3d241f180b02794442364af24c426e7
Himeth Dahanayake <himethdaha@gmail.com>
2022-02-09 09:23:56 -0500

    Wrote a script to add and delete the test-json data to the database

---
+++ dev-data/data/import-data-script.js
@@ -0,0 +1,63 @@
+//Script to import test JSON data to the database
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Tour = require('../../models/tourModel');
+
+//Get the config variables
+dotenv.config({ path: './config.env' });
+
+//Read the data from the file
+const data = JSON.parse(
+  fs.readFileSync('./dev-data/data/test-data.json', 'utf-8')
+);
+
+//Create the db string
+const DbString = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+//Connect to the database
+mongoose
+  .connect(DbString, {
+    //To deal with deprecation warnings
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then((conn) => {
+    console.log('Connected to the db');
+  });
+
+//Post all the test data to the database
+const importData = async () => {
+  try {
+    await Tour.create(data);
+    console.log('Data Imported Successfully');
+    process.exit();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//Method to delete the data
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Deleted successfully');
+    process.exit();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//Run import if the 3rd command is '--import' in the cmd when using process.argv
+if (process.argv[2] === '--import') {
+  importData();
+}
+//Run delete if the 3rd command is '--import' in the cmd when using process.argv
+if (process.argv[2] === '--delete') {
+  deleteData();
+}
--- models/tourModel.js
+++ models/tourModel.js
@@ -7,13 +7,55 @@ const tourSchema = new mongoose.Schema({
     required: [true, 'A Name is required'],
     unique: true,
   },
+  duration: {
+    type: Number,
+    required: [true, 'A duration is required'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A group must have at least 1 person'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A difficulty level is required'],
+  },
+  ratingsAverage: {
+    type: Number,
+  },
+  ratingsQuantity: {
+    type: Number,
+  },
   price: {
     type: Number,
-    required: [true, 'A Price is required'],
+    required: [true, 'A price is required'],
   },
-  rating: {
-    type: Number,
-    default: 4.0,
+  summary: {
+    type: String,
+    required: [true, 'A summary is required'],
+  },
+  description: {
+    type: String,
+    required: [true, 'At least a 100 word description is required'],
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'An image cover is required'],
+  },
+  images: [String],
+  startDates: [String],
+  expeditionOrganizer: {
+    type: String,
+    required: [true, 'Name of organizer is required'],
+  },
+  expeditionGuide: {
+    type: String,
+    required: [true, 'Name of guide is required'],
+  },
+  Dangers: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+    select: false,
   },
 });
 
