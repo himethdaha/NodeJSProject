96ad73c54265c3c1c2b18e21d316fbd976e75cef
Himeth Dahanayake <himethdaha@gmail.com>
2022-03-07 18:29:47 -0500

    Implementing data sanitization

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const AppError = require('./utilis/appErrorHandler');
 
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -31,7 +33,13 @@ app.use(
   })
 );
 
-app.use(express.json());
+//Data Sanitization
+//To read req.body data
+app.use(express.json({ limit: '10kb' }));
+//Protect against NoSql injections
+app.use(mongoSanitize());
+//Protect against cross-site-scripting
+app.use(xss());
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
